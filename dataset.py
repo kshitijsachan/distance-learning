fixed normalization bugs

dataset.py
@@ -19,9 +19,6 @@ class DistanceDataset(torch.utils.data.IterableDataset):
         self.true_distance = defaultdict(lambda: random.randint(0, 500), self.true_distance)
         self.cntr = 0
 
-    def _discount_distance(self, distance):
-        return (1 - self.GAMMA ** distance) / (1 - self.GAMMA)
-
     def traj_to_data_pairs(self, traj):
         ram_traj, img_traj = self.transform(traj)
         n = len(traj)
@@ -55,9 +52,10 @@ class DistanceDataset(torch.utils.data.IterableDataset):
                     # DELETE THIS
                     k = parse_example(x)
                     if k not in self.true_distance:
+                        ipdb.set_trace()
                         self.cntr += 1
                         print(self.cntr)
 
-                    to_predict_y = self.true_distance[k]
+                    to_predict_y = self.label_mapping_func(self.true_distance[k])
                     yield (x, to_predict_y, true_y, img)
 

distance_learner.py
@@ -12,14 +12,14 @@ from utils import IterativeAverage, trajectories_generator
 
 
 class DistanceLearner():
-    def __init__(self, train_dataset, test_dataset, label_mapper, num_classes, savedir, learning_rate=1e-4, batch_size=128, epochs=1, device=None, train_episodes=100, test_episodes=30):
+    def __init__(self, train_dataset, test_dataset, label_mapper, savedir, learning_rate=1e-4, batch_size=128, epochs=1, device=None, train_episodes=100, test_episodes=30):
         self.epochs = epochs
         self.batch_size = batch_size
         if device is None:
             self.device = "cuda" if torch.cuda.is_available() else "cpu"
         else:
             self.device = device
-        self.model = DistanceNetwork(input_dim=12, output_dim=num_classes).to(self.device)
+        self.model = DistanceNetwork(input_dim=12, output_dim=1).to(self.device)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
         self.train_data = lambda: torch.utils.data.DataLoader(train_dataset, batch_size=batch_size)
         self.test_data = lambda: torch.utils.data.DataLoader(test_dataset, batch_size=batch_size)
@@ -34,20 +34,6 @@ class DistanceLearner():
         self.test_loss = [] 
 
         self.loss_fn = torch.nn.MSELoss()
-#        undiscounted_distr = [avg_length - i for i in range(avg_length)]
-#        discounted_distr = defaultdict(int)
-#        for val, cnt in enumerate(undiscounted_distr):
-#            discount_val = label_mapper(val)
-#            discounted_distr[discount_val] += cnt
-
-#        weights = []
-#        for k in range(max(discounted_distr.keys()) + 1):
-#            weights.append(discounted_distr[k])
-#        probs = torch.tensor(weights).to(self.device) / sum(weights)
-#        weights = 1 / probs
-#        print(probs)
-#        self.loss_fn = torch.nn.CrossEntropyLoss(weight=weights)
-
 
     def train_loop(self):
         loop_loss = IterativeAverage()
@@ -160,9 +146,8 @@ if __name__ == "__main__":
     with open(os.path.join(savedir, "run_command.txt"), "w") as f:
         f.write(' '.join(str(arg) for arg in sys.argv))
 
-    label_mapper = lambda x: x
-    num_classes = 1
+    label_mapper = discount_distance
     train_data = DistanceDataset(lambda: trajectories_generator(args.train_data_path), feature_extractor, label_mapper)
     test_data = DistanceDataset(lambda: trajectories_generator(args.test_data_path), feature_extractor, label_mapper)
-    learner = DistanceLearner(train_data, test_data, label_mapper, num_classes, savedir=savedir, device=args.device, epochs=args.num_epochs)
+    learner = DistanceLearner(train_data, test_data, label_mapper, savedir=savedir, device=args.device, epochs=args.num_epochs)
     learner.run()

montezuma_ram_feature_extractor.py
@@ -1,6 +1,7 @@
 import ipdb
 import numpy as np
 from collections import defaultdict
+from utils import _parse_state
 
 from feature_extractor import FeatureExtractor
 
@@ -68,7 +69,8 @@ class MonteRAMParser:
 
         vector = []
         for i, (k, v) in enumerate(state.items()):
-            if k in ['player_x', 'player_y']:
+            if k in ['player_x', 'player_y', 'skull_x', 'lives']:
+                # normalized features should be appended as floats, not ints
                 vector.append(v)
             elif k == "player_status":
                 vector.append(dict_val_to_idx(self.status_codes_dict, v))
@@ -85,8 +87,9 @@ class MonteRAMParser:
             elif k == "object_vertical_dir":
                 vector.append(int(v == "up"))
             else:
-                # v should be a bool, int, or np.uint
+                assert isinstance(v, (int, np.bool_, np.uint)), 'did not correctly vectorize state'
                 vector.append(int(v))
+
         return vector
 
     def reset(self):
@@ -98,7 +101,15 @@ class MonteRAMParser:
     def prune_for_first_room_distance_prediction(self, state):
         pruned_state = dict()
         for k in ['player_x', 'player_y', 'player_look', 'player_jumping', 'player_falling', 'score', 'lives', 'door_left', 'door_right', 'has_skull', 'skull_x', 'skull_dir', 'respawning', 'just_died']:
-            pruned_state[k] = state[k]
+            # normalize x, y
+            if k in ['player_x', 'skull_x']:
+                pruned_state[k] = (state[k] - 1) / 151
+            elif k == 'player_y':
+                pruned_state[k] = (state[k] - 148) / 104
+            elif k == 'lives':
+                pruned_state[k] = state[k] / 5.
+            else:
+                pruned_state[k] = state[k]
         pruned_state['has_key'] = state['inventory']['key'] > 0
         pruned_state['on_rope'] = state['player_status'] in ['on-rope', 'climbing-rope']
         pruned_state['on_ladder'] = state['player_status'] in ['on-ladder', 'climbing-ladder']
@@ -107,11 +118,6 @@ class MonteRAMParser:
     def prune_for_proof_of_concept(self, state):
         pruned_state = dict()
         for k in ['player_x', 'player_y', 'lives', 'has_key', 'on_rope', 'on_ladder']:
-            if k == 'player_x':
-                pruned_state[k] = (state[k] - 1) / 151
-            elif k == 'player_y':
-                pruned_state[k] = (state[k] - 148) / 104
-            else:
                 pruned_state[k] = state[k]
         return pruned_state
 